bbe3141e9675d4a76409bf6692b024ee46ab15a2
Yeimipao <yeimipao1@gmail.com>
2022-09-25 18:30:50 -0300

    ya se puede eliminar empresas

--- src/main/java/com/SoftFactory/proyecto/cotroller/Controlador.java
+++ src/main/java/com/SoftFactory/proyecto/cotroller/Controlador.java
@@ -7,10 +7,14 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 public class Controlador {
@@ -39,4 +43,36 @@ public class Controlador {
 
         return "redirect:/AgregarEmpresas";
     }
+
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        Empresa empr=empresaServi.getEmpresaById(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("empr", empr);
+        model.addAttribute("mensaje", mensaje);
+        return "editarEmpresa";
+    }
+
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(@ModelAttribute("empr") Empresa empr, RedirectAttributes redirectAttributes){
+        if(empresaServi.saveOrUpdateEmpresa(empr)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
+            return "redirect:/VerListaEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/EditarEmpresa/"+empr.getId();
+
+    }
+
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id){
+        try {
+            empresaServi.deleEmpresa(id);
+        }catch(Exception e){
+                return "redirect:/VerListaEmpresas";
+            }
+                return "redirect:/VerListaEmpresas";
+
+        }
 }
