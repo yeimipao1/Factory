308bf2730d566bb2d76f98d8f1453cda686dd3a0
Yeimipao <yeimipao1@gmail.com>
2022-09-26 23:57:05 -0300

    controler empleado

--- src/main/java/com/SoftFactory/proyecto/cotroller/Controlador.java
+++ src/main/java/com/SoftFactory/proyecto/cotroller/Controlador.java
@@ -1,6 +1,8 @@
 package com.SoftFactory.proyecto.cotroller;
 
+import com.SoftFactory.proyecto.entidades.Empleado;
 import com.SoftFactory.proyecto.entidades.Empresa;
+import com.SoftFactory.proyecto.servicios.EmpleadoServi;
 import com.SoftFactory.proyecto.servicios.EmpresaServi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
@@ -20,7 +22,7 @@ import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 public class Controlador {
     @Autowired
     EmpresaServi empresaServi;
-
+    EmpleadoServi empleadoServi;
     @GetMapping ({"/","/VerListaEmpresas"})
     public String viewEmpresas(Model model){
         List<Empresa> listaEmpresas=empresaServi.getAllEmpresa();
@@ -75,4 +77,76 @@ public class Controlador {
                 return "redirect:/VerListaEmpresas";
 
         }
+
+    @GetMapping("/VerListaEmpleados")
+    public String verEmpresas(Model model, @ModelAttribute("mensaje") String mensaje) {
+        List<Empleado> listaEmpleados = empleadoServi.getAllEmpleados();
+        model.addAttribute("listEmpl", listaEmpleados);
+        model.addAttribute("mensaje", mensaje);
+        return "verListaEmpleados"; //llamamos al HTML
+    }
+
+    @GetMapping("/AgregarEmpleado")
+    public String newEmpleado(Model model, @ModelAttribute ("mensaje") String mensaje) {
+        Empleado empl = new Empleado();
+        model.addAttribute("empl", empl);
+        model.addAttribute("mensaje", mensaje);
+        List<Empresa> listEmpresas = empresaServi.getAllEmpresa();
+        model.addAttribute("empList", listEmpresas);
+        return "agregarEmpleado";
+    }
+
+    @PostMapping("/GuardarEmpleado")
+    public String guardarEmpleado(Empleado empl, RedirectAttributes redirectAttributes) {
+        if(empleadoServi.saveOrUpdateEmpleado(empl)) {
+            redirectAttributes.addFlashAttribute("mensaje", "guaradado");
+            return "redirect:/VerListaEmpleados";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "errorGuardar" );
+        return "redirect:/AgregarEmpleados";
+    }
+
+
+    @GetMapping ("/EditarEmpleado/{id}")
+    public String editarEmpleado (Model model, @PathVariable Integer id, @ModelAttribute ("mensaje") String mensaje) {
+        Empleado empl = empleadoServi.getEmpleadoById(id);
+        model.addAttribute ("empl" , empl );
+        model.addAttribute("mensaje", mensaje);
+        //Para ver las empresas en el HTML
+        List<Empresa> listaEmpresas= empresaServi.getAllEmpresa();
+        model.addAttribute("empList",listaEmpresas);
+        return "editarEmpleado" ;
+    }
+
+
+    @PostMapping("/ActualizarEmpleado")
+    public String actualizarEmpresa(@ModelAttribute("empl") Empleado empl, RedirectAttributes redirectAttributes){
+        if(empleadoServi.saveOrUpdateEmpleado(empl)){
+            redirectAttributes.addFlashAttribute("mensaje","actualizado");
+            return "redirect:/VerListaEmpleados";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","Error al actualizar la empresa");
+        return "redirect:/EditarEmpleado/" + empl.getId();
+    }
+
+
+    @GetMapping("/EliminarEmpleado/{id}")
+    public String eliminarEmpleado(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        if(empleadoServi.deleteEmpleado(id)){
+            redirectAttributes.addFlashAttribute("mensaje","eliminado");
+            return "redirect:/VerListaEmpleados";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","errorDelete");
+        return "redirect:/VerListaEmpleados";
+    }
+
+    //Filtrar empleados por empresa
+    @GetMapping("/Empresa/{id}/Empleados")
+    public String verEmpleadosByEmpresa(@PathVariable("id") Integer id, Model model){
+        List<Empleado> listaEmpleados = empleadoServi.empleadosByEmpresa(id);
+        model.addAttribute("listEmpl", listaEmpleados);
+
+        return "verListaEmpleados";
+    }
+
 }
